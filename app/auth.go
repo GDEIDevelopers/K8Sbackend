ca2c8344aa74235b72dac24e84cff09a2fed7232
GuoHaoLan <coopticks@hotmail.com>
2023-12-18 15:27:24 +0800

    allow cors

--- app/auth.go
+++ app/auth.go
@@ -24,6 +24,7 @@ func (s *Server) UseCORS() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		ctx.Next()
 	}
 }
 
